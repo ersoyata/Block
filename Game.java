ffcca2ec674bd09552cc7e35650aebcdfe5a0f82
ersoybaki <ersoyatabaki@gmail.com>
2023-10-19 17:24:16 +0200

    19octGame

---
+++ Game.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import javax.swing.*;
+/**
+ * no.
+ */
+
+public class Game {
+    
+    JFrame frame;
+    JPanel panel;
+    private Table table = new Table();
+    private GameThread thread = new GameThread(table);
+
+
+
+    void buildGame() {
+        frame = new JFrame("Math tetris"); // h.mohayeji.nasrabadi@tue.nl
+        frame.setSize(600, 600);
+        frame.add(table);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    public void run() {
+        buildGame();
+        table.generateBlock();
+        thread.start();
+    }
+
+    public static void main(String[] args) {
+        new Game().run();
+    }
+}
